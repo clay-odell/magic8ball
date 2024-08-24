1f0bd316224714cea85738229e8937908c365d6e
Clay O'Dell <clay.odell.music@gmail.com>
2024-08-23 22:58:46 -0500

    final commit

--- src/Eightball.jsx
+++ src/Eightball.jsx
@@ -3,26 +3,41 @@ import eightballMsgs from './messages.json';
 import { selection } from './random.js';
 import './Eightball.css';
 
-
 const Eightball = ({ messages = eightballMsgs }) => {
-    const [message, setMessage] = useState({
-        msg: "Ask the Magic 8 Ball",
-        color: "black"
+  const [message, setMessage] = useState({
+    msg: "Ask the Magic 8 Ball",
+    color: "black"
+  });
+  const [formVisibility, setFormVisibility] = useState(false);
+
+  const handleClick = () => {
+    setMessage(selection(messages));
+    setFormVisibility(true);
+  };
+
+  const handleReset = (evt) => {
+    evt.preventDefault();
+    setMessage({
+      msg: "Ask the Magic 8 Ball",
+      color: "black"
     });
+    setFormVisibility(false);
+  };
 
-    const handleClick = (evt) => {
-        setMessage(selection(messages));
-    };
-
-    return (
-        <div
-            className="Eightball"
-            onClick={handleClick}
-            style={{ backgroundColor: message.color }}
-        >
-            {message.msg}
-        </div>
-    );
+  return (
+    <div
+      className="Eightball"
+      onClick={handleClick}
+      style={{ backgroundColor: message.color }}
+    >
+      {message.msg}
+      {formVisibility && (
+        <form className="resetButton" onSubmit={handleReset}>
+          <button type="submit">Reset</button>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default Eightball;
